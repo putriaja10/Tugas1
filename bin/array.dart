03f7545ab19ace2a27ff6dad8556bd481e28e60f
putriaja10 <putrisetyowati34.gmail.com>
2023-09-27 16:52:29 +0800

    first commit

---
+++ bin/array.dart
@@ -0,0 +1,8 @@
+void main() {
+  var nama = ["Deni", "Johana", "putri", "lala"];
+  var umur = [10, 20, 20, 30];
+
+  var getName = nama[0];
+  var usia = umur[0];
+  print("Nama Saya adalah $getName cantik" + " umur saya $usia Tahun");
+}
---
+++ bin/hello.dart
@@ -0,0 +1,13 @@
+void main() {
+  String nama = "Deni";
+  int umur = 20;
+  String menikah = "ayang";
+  double tinggi = 184.9;
+  var alamat = "Banjarmasin";
+
+  print("Nama Saya $nama");
+  print("Umur Saya $umur ");
+  print("Saya Menikah Bersama $menikah");
+  print("Tinngi Saya $tinggi m");
+  print("Alamat Saya $alamat");
+}
