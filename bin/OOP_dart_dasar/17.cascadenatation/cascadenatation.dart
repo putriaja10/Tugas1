fc3fe7eb985cfcf11721720d59ddafe9b0a92cbc
putriaja10 <putrisetyowati34.gmail.com>
2023-10-10 21:07:47 +0800

    17

---
+++ bin/OOP_dart_dasar/17.cascadenatation/cascadenatation.dart
@@ -0,0 +1,12 @@
+class User {
+  String? username;
+  String? name;
+  String? email;
+}
+
+void main() {
+  var user = User();
+  user.username = "putri";
+  user.name = "putri pucuk";
+  user.email = "putri@gmail.com";
+}
