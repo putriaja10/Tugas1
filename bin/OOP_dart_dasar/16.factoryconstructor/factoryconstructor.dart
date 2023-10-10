92b58694a62c65ee22c00ada87027eee078d641d
putriaja10 <putrisetyowati34.gmail.com>
2023-10-10 21:03:55 +0800

    16

---
+++ bin/OOP_dart_dasar/16.factoryconstructor/factoryconstructor.dart
@@ -0,0 +1,17 @@
+class Database {
+  Database() {
+    print('create new database');
+  }
+  static Database database = Database();
+
+  factory Database.get() {
+    return database;
+  }
+
+  void main() {
+    var database1 = Database.get();
+    var database2 = Database.get();
+
+    print(database1 == database2);
+  }
+}
